93b2f4313f531d81a08a765d5e2c18c3a87f14e2
Martin Sevrin <mjsevrin@gmail.com>
2018-03-08 01:12:38 -0800

    class now owns pointers to Ash and game spaces

--- Game.hpp
+++ Game.hpp
@@ -2,12 +2,25 @@
 #ifndef	GAME_HPP 
 #define GAME_HPP
 
+#include "Ash.hpp"
+#include "Pokemon.hpp"
+#include "House.hpp"
+#include "Village.hpp"
+#include "OakLab.hpp"
+#include "Wilderness.hpp"
+
 class Game 
 {
 	private:
-		bool		gameStatus;	
-		int		currentTime;
-	
+		bool gameStatus;	
+		int currentTime;
+
+		Ash* ash;
+		House* home;
+		Village* pallet_town;
+		OakLab* lab;
+		Wilderness* route1;
+		
 	public:
 		//constructor & destructor
 		Game();
