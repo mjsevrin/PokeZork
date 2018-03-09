b7fc0020ef97a2fea261cbc87f97e2a287f0ec6a
Martin Sevrin <mjsevrin@gmail.com>
2018-03-08 20:20:53 -0800

    add PokeShop space

---
+++ PokeShop.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include "PokeShop.hpp"
+using std::cout;
+
+PokeShop::PokeShop(string name): Space (name)
+{
+	
+}
+
+PokeShop::~PokeShop()
+{}
+
+void PokeShop::interact()
+{
+}
---
+++ PokeShop.hpp
@@ -0,0 +1,19 @@
+
+#ifndef POKESHOP_HPP
+#define POKESHOP_HPP
+
+#include <string>
+#include "Space.hpp"
+using std::string;
+
+class PokeShop : public Space 
+{
+
+	public:
+		//constructors & destructors
+		PokeShop(string name = "PokeShop");
+		~PokeShop();
+		void interact();	
+};
+
+#endif
