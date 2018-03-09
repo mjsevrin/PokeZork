7e91e4a5ce7333c25f4f25c41c5344226151ad5e
Martin Sevrin <mjsevrin@gmail.com>
2018-03-08 20:20:04 -0800

    add PokeCenter space

---
+++ PokeCenter.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include "PokeCenter.hpp"
+using std::cout;
+
+PokeCenter::PokeCenter(string name): Space (name)
+{
+	
+}
+
+PokeCenter::~PokeCenter()
+{}
+
+void PokeCenter::interact()
+{
+}
---
+++ PokeCenter.hpp
@@ -0,0 +1,19 @@
+
+#ifndef POKECENTER_HPP
+#define POKECENTER_HPP
+
+#include <string>
+#include "Space.hpp"
+using std::string;
+
+class PokeCenter : public Space 
+{
+
+	public:
+		//constructors & destructors
+		PokeCenter(string name = "PokeCenter");
+		~PokeCenter();
+		void interact();	
+};
+
+#endif
