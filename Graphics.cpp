fb1483c0d6ed3df3b21207ba595a19946a6581c6
Martin Sevrin <mjsevrin@gmail.com>
2018-03-09 01:15:03 -0800

    add functions to display logo and game map

---
+++ Graphics.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "Graphics.hpp"
+using std::cout;
+using std::endl;
+
+void printLogo()
+{
+	cout <<"	______      _          ______            _" << endl;   
+	cout <<"	| ___ \\    | |        |___  /           | |" << endl;  
+	cout <<"	| |_/ /___ | | __ ___    / /   ___  _ __| | __" << endl;
+	cout <<"	|  __// _ \\| |/ // _ \\  / /   / _ \\| '__| |/ /" << endl;
+	cout <<"	| |  | (_) |   <|  __/ / /___| (_) | |  |   < " << endl;
+	cout <<"	\\_|   \\___/|_|\\_\\\\___|\\_____/ \\___/|_|  |_|\\_\\ " 
+		<< endl << endl ;
+}
+
+void printMap()
+{
+	cout <<"		     +--------------+" << endl;
+	cout <<"        	     |     Gym      |" << endl; 
+	cout <<"       		     +-----+-+------+" << endl; 
+	cout <<"              		   | |" << endl; 
+	cout <<"	+----------+ +-----+-+------+ +------------+" << endl; 
+	cout <<"	|          | |              | |            |" << endl; 
+	cout <<"	| PokeShop +-+  Pewter City +-+ PokeCenter |" << endl; 
+	cout <<"	|          +-+    Square    +-+            |" << endl; 
+	cout <<"	+----------+ |              | +------------+" << endl; 
+	cout <<"	             +-----+-+------+" << endl; 
+	cout <<"	     		   | |" << endl; 
+	cout <<"		     +-----+-+------+" << endl; 
+	cout <<"		     |  Wilderness  |" << endl; 
+	cout <<" 		     +-----+-+------+" << endl; 
+	cout <<" 	                   | |" << endl; 
+	cout <<"     +-------------+ +-----+-+------+ +------------+" << endl; 
+	cout <<"     |             | |              | |            |" << endl; 
+	cout <<"     | Mom's House +-+  Pallet Town +-+ PokeCenter |" << endl; 
+	cout <<"     |             +-+    Square    +-+            |" << endl; 
+	cout <<"     +-------------+ |              | +------------+" << endl; 
+	cout <<"                     +--------------+" << endl << endl;
+
+
+}
---
+++ Graphics.hpp
@@ -0,0 +1,5 @@
+
+
+void printLogo();
+
+void printMap();
