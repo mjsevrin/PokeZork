c5be5421c65fa874bde4f273509d6fb68f311970
Martin Sevrin <mjsevrin@gmail.com>
2018-03-08 20:19:35 -0800

    add Gym space

---
+++ Gym.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include "Gym.hpp"
+using std::cout;
+
+Gym::Gym(string name): Space (name)
+{
+	
+}
+
+Gym::~Gym()
+{}
+
+void Gym::interact()
+{
+}
---
+++ Gym.hpp
@@ -0,0 +1,19 @@
+
+#ifndef GYM_HPP
+#define GYM_HPP
+
+#include <string>
+#include "Space.hpp"
+using std::string;
+
+class Gym : public Space 
+{
+
+	public:
+		//constructors & destructors
+		Gym(string name = "Pewter City Gym");
+		~Gym();
+		void interact();	
+};
+
+#endif
