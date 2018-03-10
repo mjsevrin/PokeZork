4f4379f3f2a4b24cecceaf638c457eef88c07df9
Martin Sevrin <mjsevrin@gmail.com>
2018-03-10 00:16:24 -0800

    fixes circular depencies for compiler

--- House.cpp
+++ House.cpp
@@ -1,7 +1,9 @@
 
 #include <iostream>
 #include "House.hpp"
+#include "Ash.hpp"
 using std::cout;
+using std::endl;
 
 House::House(string name): Space (name)
 {
@@ -12,6 +14,22 @@ House::~House()
 {
 }
 
-void House::interact(Ash*)
+void House::interact(Ash* ash)
 {
+	if (!ash->hasPokemon())
+	{
+		cout << "MOM: Hey Ash! If you really want to prove to me you "
+			"want to be pokemon trainer, you need a " << endl <<
+			"pokemon! Go see the weird veterinarian across town, "
+			"maybe he'll have a rescue you can borrow." << endl; 
+		cout << endl;
+	}
+
+	else
+	{
+		cout << "MOM: What an adorable Pikachu! He looks weak though, "
+			"you should train along ROUTE 1, by fighting " << endl <<
+			"wild pokemons. I hate those ugly insects running around"
+		       	" there anyways." << endl << endl;
+	}
 }
