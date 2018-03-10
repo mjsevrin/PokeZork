ea54874ad43b91f478b250841e2a204353b160f3
Martin Sevrin <mjsevrin@gmail.com>
2018-03-10 00:47:02 -0800

    add fighting wild caterpie with random stats

--- Wilderness.cpp
+++ Wilderness.cpp
@@ -1,7 +1,11 @@
 
 #include <iostream>
+#include "getRand.hpp"
 #include "Wilderness.hpp"
+#include "Ash.hpp"
+#include "Pokemon.hpp"
 using std::cout;
+using std::endl;
 
 Wilderness::Wilderness(string name): Space (name)
 {
@@ -11,6 +15,12 @@ Wilderness::Wilderness(string name): Space (name)
 Wilderness::~Wilderness()
 {}
 
-void Wilderness::interact(Ash*)
+void Wilderness::interact(Ash* ash)
 {
+	Pokemon* pokemon = new Pokemon("Caterpie", getRand(20, 25), 3, 30);
+	
+	cout << "A wild CATERPIE appears!" << endl;
+
+	ash->Fight(pokemon);
+	delete pokemon;
 }
